9c823b4086a6c13cf15311b48714d884094f3ea1
Alysson Fernandes <alyss0n_F@hotmail.com>
2023-08-19 10:57:24 -0300

    feat: seller dashboard created

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,48 @@
-
-
+import { useEffect, useState } from "react"
+import { Footer } from "../../components/Footer"
+import { Header } from "../../components/Header"
+import { Card, Container, ProfileAdsList, ProfileInfo } from "./style"
+import { api } from "../../service/api"
+import jwt_decode from "jwt-decode"
 
 
 export const Dashboard = () =>{
+    const [user, setUser] = useState({})
+    const [cars, setCars] = useState([])
+    const decodedToken: any = jwt_decode(localStorage.getItem("@fipe:token"))
+
+
+    useEffect(() => {
+        (async () => {
+            const response = await api.get(`user/${decodedToken.sub}`)
+            setUser(response.data)
+
+            const response2 = await api.get("car")
+            console.log(response2.data)
+            setCars(response2.data)
+        })()
+    }, [])
+
     return(
         <>
-            <h1>DASHBOARD </h1>
+            <Header/>
+            <Container>
+                <div id="theme_color"></div>
+                <ProfileInfo>
+                    <div id="profile_image"></div>
+                    <div id="profile_name">
+                        <h1>{user.name}</h1>
+                        <span>{user.account_state == "buyer" ? "Comprador" : "Anunciante"}</span>
+                    </div>
+                    <p>{user.description}</p>
+                    <button>Criar anuncio</button>
+                </ProfileInfo>
+                <ProfileAdsList>
+                    {/* <li>{cars[0].name}</li> */}
+                   
+                </ProfileAdsList>
+            </Container>
+            <Footer/>
         </>
     )
 }
