405fd9df931b4ebcd3e9588fad6f5ca14c68affe
Alysson Fernandes <alyss0n_F@hotmail.com>
2023-08-20 12:04:09 -0300

    fix: useEffect list cars, fix protectedRoutes

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -6,24 +6,35 @@ import { api } from "../../service/api"
 import jwt_decode from "jwt-decode"
 import { ModalRegisterCar } from "../../components/Modals/ModalRegisterCar"
 import { useAuth } from "../../hooks/useAuth"
+import CardCar from "../../components/Showcase/Card"
+import { ICar } from "../../providers/AuthProvider"
 
 
 export const Dashboard = () =>{
-    const { setModal } = useAuth()
-    const [user, setUser] = useState({})
+    const { setModal, user, setUser } = useAuth()
     const [cars, setCars] = useState([])
-    const decodedToken = jwt_decode(localStorage.getItem("@fipe:token"))
-
 
     useEffect(() => {
         (async () => {
-            const response = await api.get(`user/${decodedToken.sub}`)
-            setUser(response.data)
+            try {
+                const token = localStorage.getItem("@fipe:token");
+                if(token){
+                    api.defaults.headers.common.Authorization = `Bearer ${token}`
+                    const decodedToken : unknown = jwt_decode(token);
+                    const userId = decodedToken.sub;
+                    const response = await api.get(`user/${userId}`);
+                    setUser(response.data);
 
-            const response2 = await api.get("car")
-            console.log(response2.data)
-            setCars(response2.data)
-            console.log(cars)
+                    const responseCarsByUser : ICar[] = await api.get(`car/user`)
+                    setCars(responseCarsByUser.data.data)
+                    console.log(cars)
+                }else{
+                    await api.get(`user/${null}`)
+                    //Garante nosso axios interceptor no caso de não obter token
+                }
+            } catch (error) {
+                console.error(error)
+            }
         })()
     }, [])
 
@@ -42,8 +53,9 @@ export const Dashboard = () =>{
                     <button onClick={() => setModal('registerCar')}>Criar anuncio</button>
                 </ProfileInfo>
                 <ProfileAdsList>
-           
-                   
+                    {cars.map((car) =>(
+                        <CardCar key={car.id} car={car}></CardCar> 
+                    ))}
                 </ProfileAdsList>
             </Container>
             <Footer/>
